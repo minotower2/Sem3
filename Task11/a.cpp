d28cc0374dfbd04915c06f37b2728d83bf8df3c7
minotower <minotower2@yandex.com>
2024-12-12 13:11:21 +0300

    Wrote initialization and printing of a tree

--- Task11/a.cpp
+++ Task11/a.cpp
@@ -1,29 +1,23 @@
-#include "list2.h"
 #include "student.h"
-#include "tree.h"
+#include "btree.h"
 #include <time.h>
-
-int list2::max_read = 0;
-int list2::max_print = 0;
 
 int main(int argc, char* argv[]) {
   char *filename = 0;
-  int r, k, task, res;
+  int r, k, m, task, res;
   double t;
   FILE *fp;
   io_status read;
 
-  if (!((argc == 4) &&
-    (sscanf(argv[1], "%d", &r) == 1) &&
+  if (!((argc == 5) &&
+    (sscanf(argv[1], "%d", &m) == 1) &&
+    (sscanf(argv[2], "%d", &r) == 1) &&
     (sscanf(argv[3], "%d", &k) == 1)))
   {
     printf("Usage %s\n", argv[0]);
     return -1;
   }
-  filename = argv[2];
-
-  list2::set_max_print(r);
-  list2::set_max_read(k);
+  filename = argv[4];
 
   fp = fopen(filename, "r");
   if (!fp) {
@@ -31,7 +25,7 @@ int main(int argc, char* argv[]) {
     return -2;
   }
 
-  tree<student> *birch = new tree<student>;
+  b_tree<student> *birch = new b_tree<student>(m);
   read = birch->read(fp);
   switch(read) {
     case io_status::success:
@@ -52,103 +46,15 @@ int main(int argc, char* argv[]) {
       printf("Failed to read list\n");
       delete birch;
       return -6;
+    case io_status::create:
+      printf("aaaaaaaaaaaaaaaa\n");
+      delete birch;
+      return -7;
   }
   birch->print(r);
-
-  task = 1;
-  t = clock();
-  res = birch->solve1(birch->get_root());
-  t = (clock()-t)/CLOCKS_PER_SEC;
-  printf ("%s : Task = %d M = %d Result = %d Elapsed = %.2f\n", argv[0], task, k, res, t);
-
-  task = 2;
-  t = clock();
-  res = birch->solve2(birch->get_root());
-  t = (clock()-t)/CLOCKS_PER_SEC;
-  printf ("%s : Task = %d M = %d Result = %d Elapsed = %.2f\n", argv[0], task, k, res, t);
-
-  task = 3;
-  t = clock();
-  res = birch->solve3(birch->get_root());
-  t = (clock()-t)/CLOCKS_PER_SEC;
-  printf ("%s : Task = %d M = %d Result = %d Elapsed = %.2f\n", argv[0], task, k, res, t);
-
-  task = 4;
-  t = clock();
-  res = birch->solve4(birch->get_root());
-  t = (clock()-t)/CLOCKS_PER_SEC;
-  printf ("%s : Task = %d M = %d Result = %d Elapsed = %.2f\n", argv[0], task, k, res, t);
-
-  task = 5;
-  t = clock();
-  res = birch->solve5(birch->get_root());
-  t = (clock()-t)/CLOCKS_PER_SEC;
-  printf ("%s : Task = %d M = %d Result = %d Elapsed = %.2f\n", argv[0], task, k, res, t);
-
+  (void) t;
+  (void) task;
+  (void) res;
   delete birch;
-  fclose(fp);
-
-
-  fp = fopen(filename, "r");
-  if (!fp) {
-    printf("Can't open file %s\n", filename);
-    return -2;
-  }
-  tree<list2> *pine = new tree<list2>;
-  read = pine->read(fp);
-  switch(read) {
-    case io_status::success:
-      break;
-    case io_status::eof:
-      printf("Reached end of file\n");
-      delete pine;
-      return -3;
-    case io_status::format:
-      printf("Wrong format of data\n");
-      delete pine;
-      return -4;
-    case io_status::memory:
-      printf("Not enough memory\n");
-      delete pine;
-      return -5;
-    case io_status::read:
-      printf("Failed to read list\n");
-      delete pine;
-      return -6;
-  }
-  pine->print(r);
-
-  task = 1;
-  t = clock();
-  res = pine->solve1(pine->get_root());
-  t = (clock()-t)/CLOCKS_PER_SEC;
-  printf ("%s : Task = %d M = %d Result = %d Elapsed = %.2f\n", argv[0], task, k, res, t);
-
-  task = 2;
-  t = clock();
-  res = pine->solve2(pine->get_root());
-  t = (clock()-t)/CLOCKS_PER_SEC;
-  printf ("%s : Task = %d M = %d Result = %d Elapsed = %.2f\n", argv[0], task, k, res, t);
-
-  task = 3;
-  t = clock();
-  res = pine->solve3(pine->get_root());
-  t = (clock()-t)/CLOCKS_PER_SEC;
-  printf ("%s : Task = %d M = %d Result = %d Elapsed = %.2f\n", argv[0], task, k, res, t);
-
-  task = 4;
-  t = clock();
-  res = pine->solve4(pine->get_root());
-  t = (clock()-t)/CLOCKS_PER_SEC;
-  printf ("%s : Task = %d M = %d Result = %d Elapsed = %.2f\n", argv[0], task, k, res, t);
-
-  task = 5;
-  t = clock();
-  res = pine->solve5(pine->get_root());
-  t = (clock()-t)/CLOCKS_PER_SEC;
-  printf ("%s : Task = %d M = %d Result = %d Elapsed = %.2f\n", argv[0], task, k, res, t);
-
-  delete pine;
-  fclose(fp);
   return 0;
 }
